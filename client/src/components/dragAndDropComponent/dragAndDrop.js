d2a9a5c17ec7acc2314988c22a937d75a22f5e00
Vadim <vadym.baryshnikov@globallogic.com>
2018-11-16 18:34:38 +0200

    i've finished drag and drop component

--- client/src/components/dragAndDropComponent/dragAndDrop.js
+++ client/src/components/dragAndDropComponent/dragAndDrop.js
@@ -1,12 +1,43 @@
 export class dragAndDrop {
-    constructor({areaSelector, activeDragMethod, disactiveDragMethod, dropMethod}) {
+    constructor({areaSelector=false, activeDragMethod=false, disactiveDragMethod=false, dropMethod=false, dragElementSelector=false, 
+        dragElementMethod=false, dragStartElementMethod=false, dragEndElementMethod=false}) {
         this.areaSelector = areaSelector;
         this.activeDragMethod = activeDragMethod;
         this.disactiveDragMethod = disactiveDragMethod;
         this.dropMethod = dropMethod;
-        this.setListenerActiveDragAndDrop();
-        this.setListenerDisactiveDragAndDrop();
-        this.setListenerDropEvent();
+        this.dragElementSelector = dragElementSelector;
+        this.dragElementMethod = dragElementMethod;
+        this.dragStartElementMethod = dragStartElementMethod;
+        this.dragEndElementMethod = dragEndElementMethod;
+        if(this.areaSelector) {
+            if(this.activeDragMethod) {
+                this.setListenerActiveDragAndDrop();
+            }
+            if(this.disactiveDragMethod) {
+                this.setListenerDisactiveDragAndDrop();
+            }
+            this.setListenerDropEvent();
+        }
+        if(this.dragElementSelector) {
+            if(this.dragElementMethod) {
+                this.setListenerDraggingElement();
+            }
+            if(this.dragStartElementMethod) {
+                this.setListenerStartDraggingElement();
+            }
+            if(this.dragEndElementMethod) {
+                this.setListenerEndDraggingElement();
+            }
+        }  
+    }
+    setListenerStartDraggingElement() {
+        document.querySelector(this.dragElementSelector).addEventListener('dragstart', this.dragStartElementMethod, false);
+    }
+    setListenerEndDraggingElement() {
+        document.querySelector(this.dragElementSelector).addEventListener('dragend', this.dragEndElementMethod, false);
+    }
+    setListenerDraggingElement() {
+        document.querySelector(this.dragElementSelector).addEventListener('drag', this.dragElementMethod, false);
     }
     setListenerActiveDragAndDrop() {
         ['dragenter', 'dragover'].forEach((i) => {
